c52ecb020428d15c0f0d5d397e650664b745b891
KetsuRyo <89430455+KetsuRyo@users.noreply.github.com>
2024-02-24 14:36:46 -0500

    Update views.py

--- bci_backend/analyzeapp/views.py
+++ bci_backend/analyzeapp/views.py
@@ -8,11 +8,12 @@ import os
 from django.views.decorators.csrf import csrf_exempt
 
 import json
-from .autoencoder_2 import EEGAutoencoderClassifier
+from .newEncoder import EEGAutoencoderClassifier
+
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model = EEGAutoencoderClassifier(num_classes=5, hidden_units=[1024, 512, 256])
-model_path = os.path.join(settings.BASE_DIR, 'analyzeapp', 'eeg_autoencoder_classifier_500.pth')
+model = EEGAutoencoderClassifier(num_classes=5)  # 移除 hidden_units 参数
+model_path = os.path.join(settings.BASE_DIR, 'analyzeapp', 'eeg_CNNautoencoder_classifier_72.07.pth')
 model.load_state_dict(torch.load(model_path, map_location=device))
 model.to(device)
 model.eval()
@@ -20,35 +21,37 @@ model.eval()
 @csrf_exempt
 def analyze(request):
     if request.method == 'POST':
-        # 使用 request.body 獲取原始請求體，並解析 JSON 數據
         try:
             data = json.loads(request.body.decode('utf-8'))
-            input_data = data.get('data')
+            input_data = data.get('data')  # 这里是一个包含64个列表的列表，每个子列表有795个浮点数
             user_time = data.get('time')
             user_name = data.get('UserName')
-        except json.JSONDecodeError as e:
+        except json.JSONDecodeError:
             return JsonResponse({'error': 'Invalid JSON'}, status=400)
 
-        # 確保 input_data 不是 None
         if input_data is None:
             return JsonResponse({'error': 'No data provided'}, status=400)
+
+        # 将输入数据转换为张量，并添加一个批次维度
         input_tensor = torch.Tensor(input_data).to(device)
-        input_tensor = input_tensor.view(1, -1)
-        
+        input_tensor = input_tensor.unsqueeze(0)  # 现在形状应该是 [1, 64, 795]
+
         with torch.no_grad():
             output = model(input_tensor)
             _, predicted = torch.max(output.data, 1)
             predicted_label = predicted.item()
-        
+
         response = {
             'label': predicted_label,
             'time': user_time,
             'UserName': user_name
         }
-        
+
         return JsonResponse(response)
     else:
         return JsonResponse({'error': 'This endpoint only supports POST requests.'})
+
+
 
 @csrf_exempt
 def register(request):
