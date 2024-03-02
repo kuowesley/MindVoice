ec17828709c24218f1089042e402997df29ee1bd
Fanyi Zhao <13493334+fanyi-zhao@users.noreply.github.com>
2024-03-02 06:49:23 -0500

    Deploy Django server to render.com

--- bci_backend/analyzeapp/views.py
+++ bci_backend/analyzeapp/views.py
@@ -1,4 +1,5 @@
 import re
+import uuid
 from django.http import JsonResponse
 from django.conf import settings
 import torch
@@ -9,12 +10,11 @@ import os
 from django.views.decorators.csrf import csrf_exempt
 
 import json
+import replicate
+# load auth token from .env file
+from dotenv import load_dotenv
+load_dotenv()
 
-from model.predict import Predictor
-
-predictor = Predictor()
-predictor.set_model_dir(os.path.join(settings.BASE_DIR, 'model'))
-predictor.setup()
 
 @csrf_exempt
 def analyze(request):
@@ -32,7 +32,7 @@ def analyze(request):
             return JsonResponse({'error': 'No data provided'}, status=400)
 
         response = {
-            'label': predictor.predict_local(input_data),
+            'label': predict_remote(input_data),
             'time': user_time,
             'UserName': user_name
         }
@@ -41,7 +41,24 @@ def analyze(request):
     else:
         return JsonResponse({'error': 'This endpoint only supports POST requests.'})
 
-
+def predict_remote(eeg_data) -> int:
+        # generate file name based on the hash of the data
+        file_name = str(uuid.uuid4()) + "-" + str(hash(str(eeg_data))) + '.json'
+        json_data = {
+            "data": eeg_data
+        }
+        # save to a temp file
+        try:
+            with open(file_name, 'w') as f:
+                json.dump(json_data, f)
+            with open(file_name, 'rb') as f:
+                output = replicate.run(
+                    "fanyi-zhao/bci-backend:977ca5966281eeaf1cd7ee40950cafd38cd855cecc6678e71caefdf452d24a42",
+                    input={"eeg_data": f},
+                )
+            return int(output)
+        finally:
+            os.remove(file_name)
 
 @csrf_exempt
 def register(request):
