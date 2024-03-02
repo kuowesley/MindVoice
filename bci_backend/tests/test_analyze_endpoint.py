e5243b4018431804c3878565c47b2e729a0c9ce9
Fanyi Zhao <13493334+fanyi-zhao@users.noreply.github.com>
2024-03-02 01:08:34 -0500

    move request data to class field

--- bci_backend/tests/test_analyze_endpoint.py
+++ bci_backend/tests/test_analyze_endpoint.py
@@ -4,10 +4,10 @@ from django.test import TestCase, Client
 import json
 
 """
-Testing the analyze endpoint
+Test the /api/analyze/ endpoint
 """
-from bciBackend import asgi, wsgi, urls, settings
 class AnalyzeDataTestCase(TestCase):
+    from bciBackend import asgi, wsgi, urls, settings
     def setUp(self):
         self.label_mapping = {
             'hello.json': 0,
@@ -16,29 +16,30 @@ class AnalyzeDataTestCase(TestCase):
             'thank_you.json': 3,
             'yes.json': 4
         }
+        self.data = {
+            'data': [],
+            'time': '2024-03-01T12:00:00',
+            'UserName': 'test_user'
+        }
         self.client = Client()
 
     def load_request_data(self, file_name):
         with open(os.path.join(settings.BASE_DIR, 'tests', file_name)) as f:
-            data = {
-                'data': json.load(f).get('data'),
-                'time': '2024-03-01T12:00:00',
-                'UserName': 'test_user'
-            }
-        return data
+            self.data['data'] = json.load(f).get('data')
+            print(self.data['data'])
     
-    def assert_response_valid(self, response_data, data):
+    def assert_response_valid(self, response_data):
         self.assertIn('label', response_data)
         self.assertIn('time', response_data)
         self.assertIn('UserName', response_data)
-        self.assertEqual(response_data['time'], data['time'])
-        self.assertEqual(response_data['UserName'], data['UserName'])
+        self.assertEqual(response_data['time'], self.data['time'])
+        self.assertEqual(response_data['UserName'], self.data['UserName'])
 
     def test_analyze_data_endpoint(self):
         for file_name, label in self.label_mapping.items():
-            data = self.load_request_data(file_name)
-            response = self.client.post('/api/analyze/', json.dumps(data), content_type='application/json')
+            self.load_request_data(file_name)
+            response = self.client.post('/api/analyze/', json.dumps(self.data), content_type='application/json')
             self.assertEqual(response.status_code, 200)
             response_data = json.loads(response.content)
-            self.assert_response_valid(response_data, data)
+            self.assert_response_valid(response_data)
             self.assertEqual(response_data['label'], label)
